f4174ea1eacf0f98f3eff0fe467899798243065f
lengdiao <lengdiao1995@163.com>
2019-09-10 15:59:49 +0800

    1

--- src/main/java/com/ecard/controller/CloudPassInfoController.java
+++ src/main/java/com/ecard/controller/CloudPassInfoController.java
@@ -12,7 +12,7 @@ import io.swagger.annotations.ApiOperation;
 
 @RestController
 @CrossOrigin
-@Api(value = "登录相关接口1" )
+@Api(value = "登录相关接口" )
 @RequestMapping("/cloudPassinfo")
 public class CloudPassInfoController {
     @Autowired
